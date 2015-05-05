c0e0f54055a41c0b310ffc05781f32176dbae68b
Taras Iagniuk <mrtaryk@gmail.com>
2015-05-05 07:55:31 +0300

    Specify a user-wide profile for leiningen

---
+++ .lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [refactor-nrepl "1.0.5"]]
+        :dependencies [[org.clojure/clojure "1.6.0"]
+                       [org.clojure/tools.nrepl "0.2.10"]]}}
