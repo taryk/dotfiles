6e9b64a19ab678c7ea7fddcd2c756db658232663
Taras Iagniuk <mrtaryk@gmail.com>
2015-11-21 07:40:08 +0200

    Clojure 1.7.0 is the current stable version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:dependencies [[org.clojure/clojure "1.6.0"]]}}
+{:user {:dependencies [[org.clojure/clojure "1.7.0"]]}}
 {:repl {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [refactor-nrepl "1.0.5"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
