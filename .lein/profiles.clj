db3bc77b8f3eda7b4d1245a798ae6c22bcf68c74
Taras Iagniuk <mrtaryk@gmail.com>
2015-11-21 07:43:32 +0200

    Update the versions of * cider/cider-nrepl to 0.9.1 * refactor-nrepl to 1.1.0 * org.clojure/tools.nrepl to 0.2.11

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:dependencies [[org.clojure/clojure "1.7.0"]]}}
-{:repl {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [refactor-nrepl "1.0.5"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
+{:repl {:plugins [[cider/cider-nrepl "0.9.1-SNAPSHOT"]
+                  [refactor-nrepl "1.1.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.11"]]}}
