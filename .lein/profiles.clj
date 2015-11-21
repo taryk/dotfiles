6d30d7ddbab64840125ebe6d30dab8c9efa17c2e
Taras Iagniuk <mrtaryk@gmail.com>
2015-11-21 07:37:16 +0200

    Move nrepl plugins/dependencies to the `repl` profile to make them available only when repl is actually run

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+{:user {:dependencies [[org.clojure/clojure "1.6.0"]]}}
+{:repl {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [refactor-nrepl "1.0.5"]]
-        :dependencies [[org.clojure/clojure "1.6.0"]
-                       [org.clojure/tools.nrepl "0.2.10"]]}}
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
